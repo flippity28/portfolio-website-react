ac12f77048c8cd48a45b74d7e1f43bf2baabbb20
flippity28 <70317116+flippity28@users.noreply.github.com>
2020-11-30 19:46:05 +0000

    updated to hide menu after selection

--- src/menu.js
+++ src/menu.js
@@ -8,17 +8,22 @@ function showMenu (event){event.preventDefault();
   if (nav){openNav(false)} else {
 openNav(true)}}
 
+function hideMenu (){
+  if (nav){openNav(false)} else {
+openNav(true)}}
+
+
 if (nav) {
 return(
       <div className="side-menu col-1">
         <div classname="row"><button onClick={showMenu}><i class="fas fa-times"></i></button></div>
           <div className="row menu-link">
-            <a  id="about-me-link" href="#about-section"><button class="link-button">About</button></a>
+            <a  id="about-me-link" href="#about-section"><button class="link-button" onClick={hideMenu}>About</button></a>
           </div>
           <div className="row menu-link">
-            <a id="projects-link" href="#projects"><button class="link-button">Projects</button></a>
+            <a id="projects-link" href="#projects"><button class="link-button" onClick={hideMenu}>Projects</button></a>
           </div>
-          <div className="row menu-link"><a id="contact-link" href="#contact"><button class="link-button">Contact</button></a></div>
+          <div className="row menu-link"><a id="contact-link" href="#contact"><button class="link-button" onClick={hideMenu}>Contact</button></a></div>
           
         </div>
     
